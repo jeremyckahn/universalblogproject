a52ffd81a21d5040a11439d7db97dbdd586dbc6b
jeremyckahn <jeremyckahn@49d7819a-fb50-11de-a20f-79d6ecec5795>
2010-01-11 22:47:55 +0000

    Starting toying around with some AJAX stuff to load data dynamically.  Unfinished code is commented out.

--- trunk/system/application/views/blogView.php
+++ trunk/system/application/views/blogView.php
@@ -12,4 +12,56 @@
     </div>
 	<? } ?>
 <? } ?>
+
 </form>
+
+<script type="text/javascript">
+	// Some experimental code I'm not done with.  Never you mind this.
+
+	// Function taken from W3Schools.  http://www.w3schools.com/ajax/ajax_example_suggest.asp
+	/*function getXmlHttpRequestObject()
+	{
+		if (window.XMLHttpRequest)
+		{
+		  // code for IE7+, Firefox, Chrome, Opera, Safari
+		  return new XMLHttpRequest();
+		}
+		if (window.ActiveXObject)
+		{
+		  // code for IE6, IE5
+		  return new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		return null;
+	}
+	
+	var blogXHR = getXmlHttpRequestObject();
+	function loadMorePosts()
+	{	
+		if (blogXHR == null)
+		{
+			alert("Hmmm... either you are using a very old browser, or you have incredibly strict security settings.  Your browser won't let you load posts.");
+			return;	
+		}
+		
+		//var url = "scripts/php/feed/feedFormatter.php";
+		var url = "<?= $this->config->item('base_url') . "index.php/" . $this->uri->segment(1) . "/blogLoader"; ?>";
+		var parameters = "requestSize=" + 5;
+		parameters += "&startFrom=" + 1;
+		parameters += "&userID=" + 1;
+		parameters += "&sid=" + Math.random();
+		
+		blogXHR.onreadystatechange = loadBlogsStateChanged;
+		
+		blogXHR.open("POST", url, true);
+		blogXHR.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		blogXHR.send(parameters);
+	}
+	
+	function loadBlogsStateChanged()
+	{
+		if (blogXHR.readyState == 4)
+		{
+			alert(blogXHR.responseText);
+		}	
+	}*/
+</script>
