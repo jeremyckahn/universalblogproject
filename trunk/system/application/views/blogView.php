a05beca6842c1e02f1f038c060d6afc4aa0ef345
jeremyckahn <jeremyckahn@49d7819a-fb50-11de-a20f-79d6ecec5795>
2010-01-11 13:59:46 +0000

    Started implementing blacklisting functionality on login, not fully implemented yet.

--- trunk/system/application/views/blogView.php
+++ trunk/system/application/views/blogView.php
@@ -1,8 +1,13 @@
 <p>This is the blog view.</p>
 
+<?= form_open('ubp/index'); ?>
 <? foreach ($postArray as $post){ ?>
-	<?= htmlentities(urldecode($post['title'])) ?>
+	<?= htmlentities(urldecode($post['title'])) . "\n" ?>
 	<br/>
-	<?= htmlentities(urldecode($post['post'])) ?>
+	<?= htmlentities(urldecode($post['post'])) . "\n" ?>
+	<? if($this->session->userdata("loggedIn")){ ?>
+	<button type="submit" name="blacklistButton" type="button" value="<?= $post['blogID'] ?>">Blacklist this post</button>
+	<? } ?>
 	<br/><br/>
 <? } ?>
+</form>
