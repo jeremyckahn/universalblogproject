e117ea35941e5bf88acdafe7d9c1aa6a2b7f5aea
jeremyckahn <jeremyckahn@49d7819a-fb50-11de-a20f-79d6ecec5795>
2010-01-14 06:03:55 +0000

    Made and implemented a general AJAX JS adapter for re-use with other objects.

--- trunk/js/blogManager.js
+++ trunk/js/blogManager.js
@@ -1,27 +1,20 @@
-var content, blogXHR, blogArray;
-
-function blogManager(blogDiv, xhrObject){
-	blogXHR = xhrObject;
-	content = blogDiv;
-	blogArray = new Array();
+function blogManager(){
+	var blogArray = new Array();
 	
-	this.loadMorePosts = function(serverScriptURL, requestSize, startFrom, userID){
-		if (blogXHR == null)
-		{
-			alert("Uh oh!  Unable to access the server.");
-			return;	
-		}
-		
+	this.loadMorePosts = function(serverScriptURL, requestSize, startFrom, userID, blogContainer){
+		var content = blogContainer;
 		var url = serverScriptURL.toString();
 		var parameters = "requestSize=" + requestSize.toString();
 		parameters += "&startFrom=" + startFrom.toString();
 		parameters += "&userID=" + userID.toString();
 		parameters += "&sid=" + Math.random();
 		
-		blogXHR.onreadystatechange = function(){
-			if (blogXHR.readyState == 4)
+		var adapter = new ajaxAdapter(url, parameters);
+		
+		this.eventHandler = function(){
+			if (adapter.xhr.readyState == 4)
 			{
-				content.innerHTML = (blogXHR.responseText);
+				content.innerHTML = (adapter.xhr.responseText);
 				var blogList = document.getElementById("blogList");
 				
 				blogArray = blogList.value.split("_");
@@ -30,8 +23,6 @@ function blogManager(blogDiv, xhrObject){
 			}
 		};
 		
-		blogXHR.open("POST", url, true);
-		blogXHR.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		blogXHR.send(parameters);
+		adapter.send(adapter.xhr, this.eventHandler);
 	};
 }
