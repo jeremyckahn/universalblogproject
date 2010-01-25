9c77964fc5c0fe45fe3e749d89686430e4fc7b29
jeremyckahn <jeremyckahn@49d7819a-fb50-11de-a20f-79d6ecec5795>
2010-01-25 00:11:06 +0000

    Began implementing CSS from old site.  Not done.

--- system/application/views/templateEnd.php
+++ system/application/views/templateEnd.php
@@ -1,2 +1,6 @@
-<?= "\n    " ?></body>
+<?= "\n    " ?>
+		</div> <!-- /titleAndContentContainer -->
+	</body>
 </html>
+
+<!-- Universal Blog Engine created by Jeremy Kahn, 2010.  This version is built with the CodeIgniter framework:  http://codeigniter.com/ -->
