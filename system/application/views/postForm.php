f46a2625e09aa7ee1bd073f86e584628bf4102fc
jeremyckahn <jeremyckahn@49d7819a-fb50-11de-a20f-79d6ecec5795>
2010-01-30 21:47:13 +0000

    CSS and JS retrofitting.

--- system/application/views/postForm.php
+++ system/application/views/postForm.php
@@ -1,18 +1,38 @@
-<p>This is the post form.</p>
+<? if($this->session->userdata("loggedIn")){ 
 	
-	<? if($this->session->userdata("loggedIn")){ ?>
+	$formAttributes = array(
+					"id" => "postForm",
+					"class" => "formContainer");
+?>
+
+<?= form_open('ubp/post', $formAttributes); ?>
+	<div class ="label">Title:</div>
+
+	<input id="txtTitle" class="txtTitle" type="text" name="title" value="<?= set_value('title'); ?>"/>
+			
+	<div class ="label">Post:</div>
+
+	<textarea id = "postInput" class="txtPost" name="post" rows="10" cols="30"><?= set_value('post'); ?></textarea>
 
 	<div style="color: #f00;"><?= validation_errors(); ?></div>
-	<?= form_open('ubp/post'); ?>
-		<div class ="label">Title:</div>
-		<input id="txtTitle" class="txtTitle" type="text" name="title" value="<?= set_value('title'); ?>"/>
-				
-		<div class ="label">Post:</div>
-		<textarea id = "postInput" class="txtPost" name="post" rows="10" cols="30"><?= set_value('post'); ?></textarea>
-		
-		<input type="submit" value="Submit" />
-	</form>
 	
-	<?} else {?>
-		<p>You need to be logged in to make a post.</p>
-	<? } ?>
+	<input type="submit" value="Submit" />
+	
+	<!--<div class="customUIButtonFrame">
+		
+		<span class="button rollover" onclick="preview();">preview</span>
+	
+	</div>-->
+	
+</form>
+
+<script type=text/javascript">
+	function preview()
+	{
+		
+	}
+</script>
+
+<?} else {?>
+	<p>You need to be logged in to make a post.</p>
+<? } ?>
