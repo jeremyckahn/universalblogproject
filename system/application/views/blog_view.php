447328386c0354adf02bf055e6b9473d89347866
jeremyckahn <jeremyckahn@49d7819a-fb50-11de-a20f-79d6ecec5795>
2010-03-22 04:55:56 +0000

    Completely re-did the way blogs are loaded. (Issues 4 and 9)

--- system/application/views/blog_view.php
+++ system/application/views/blog_view.php
@@ -14,12 +14,39 @@
 	<span id="btnPageForward" class="paginationButtonMore rollover" onclick="loadMorePosts();">more</span>
 </div>
 
+<div id="blogTemplate" class="hidden">
+
+	<div id="postID_0postID0" class="postContainer">
+	
+		<h1 class="articleHeader">
+		
+			<a href="<?php echo base_url(); ?>index.php/ubp/index/blogID/0postID0">0postTitle0</a>
+			
+		</h1>
+	
+		<p>0postBody0</p>
+	
+		<?php if ($this->session->userdata("loggedIn")):?>
+		<div class="controlPanel">
+		
+			<span class="blacklistButton bracketize rollover" value="0postID0" onclick="blacklist(0postID0)">blacklist this post</span>
+			
+		</div>
+		<?php endif; ?>
+	
+	</div>
+	
+</div>
+
 <script type="text/javascript" src="<?php echo base_url() . "js/blogManager.js"; ?>"></script>
 
 <script type="text/javascript">
 /* <![CDATA[ */
 	var manager = new blogManager();
 	var blogContainer = document.getElementById("mainFeed");
+	var blogTemplate = document.getElementById("blogTemplate");
+	
+	manager.setPostTemplate(manager, blogTemplate);
 	
 	function loadMorePosts(requestSize)
 	{
