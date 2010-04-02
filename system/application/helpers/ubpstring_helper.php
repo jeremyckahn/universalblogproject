15d9514d872e3090b89a5e652065af6703fad75d
jeremyckahn <jeremyckahn@49d7819a-fb50-11de-a20f-79d6ecec5795>
2010-04-02 13:40:44 +0000

    Fixed <br/> issue.  Bizarre Chrome bug not fixed.

--- system/application/helpers/ubpstring_helper.php
+++ system/application/helpers/ubpstring_helper.php
@@ -1,5 +1,13 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+if ( ! function_exists('preserveBRs'))
+{
+	function preserveBRs($string)
+	{
+		return str_replace("&lt;br/&gt;", "<br/>", $string);
+	}	
+}
+
 if ( ! function_exists('strContains'))
 {
 	function strContains($haystack, $needle)
--- system/application/views/single_post_view.php
+++ system/application/views/single_post_view.php
@@ -4,6 +4,6 @@
 	
 	<h3 class="articleDate bracketize"><?php echo date("g:i:s A - l F j, Y", strtotime($post["datePosted"])); ?></h3>
 	
-	<p><?php echo htmlentities(urldecode($post['post'])); ?></p>
+	<p><?php echo preserveBRs(htmlentities(urldecode($post['post']))); ?></p>
 	
 </div>
