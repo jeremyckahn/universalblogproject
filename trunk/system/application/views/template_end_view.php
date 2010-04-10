aeae085a6cc84138118a659be3c67edb5419a0d6
jeremyckahn <jeremyckahn@49d7819a-fb50-11de-a20f-79d6ecec5795>
2010-04-10 01:45:16 +0000

    Fixed Google Analytics tracking code.

--- trunk/system/application/views/template_end_view.php
+++ trunk/system/application/views/template_end_view.php
@@ -12,6 +12,11 @@
 			<p class="footerCredits">designed and developed by <a href = "http://www.jerbils.com/" target="_blank">Jeremy Kahn</a> &copy;</p>
 			
 		</div> <!-- /footer -->
+		<!-- Google Analytics --> 
+		<script type="text/javascript"> 
+			var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+			document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+		</script> 
 		<script type="text/javascript"> 
 			try 
 			{
